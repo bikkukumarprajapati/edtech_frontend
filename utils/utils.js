5afaf97292626e57f0254f00e53d08543215836e
Bikku Kumar <bikkukumar865129@gmail.com>
2025-06-20 22:06:46 +0530

    chnage url

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "http://localhost:4001/api/v1";
-export const BACKEND_URL = "https://backend-ed.onrender.com";
+export const BACKEND_URL = "http://localhost:4001/api/v1";
+// export const BACKEND_URL = "https://backend-ed.onrender.com/api/v1";
